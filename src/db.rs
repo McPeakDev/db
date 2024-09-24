802538d6933cff6f3139884e5f14600c67c2f1f1
Matthew McPeak <m.mcpeak98@icloud.com>
2024-09-23 21:48:01 -0400

    refactor(package): Make the DB package more generic

--- src/db.rs
+++ src/db.rs
@@ -1,127 +1,143 @@
-use crate::types::{DBResult, DatabaseConnection, PostgresDBPool, QueryParams, DB};
-use axum::http::StatusCode;
+use crate::types::{DBResult, PostgresDBPool, QueryParams};
 use bb8::Pool;
 use bb8_postgres::PostgresConnectionManager;
-use tokio_postgres::{row::Row, types::ToSql, Error, NoTls};
+use tokio_postgres::{row::Row, Error, GenericClient, NoTls};
 use tracing::{debug, error};
 
-pub async fn setup(config: &DB) -> PostgresDBPool {
-    let manager = PostgresConnectionManager::new_from_stringlike(
-        format!(
-            "host={} user={} password={}",
-            config.host, config.user, config.password
-        ),
-        NoTls,
-    )
-    .unwrap();
-    let pool = Pool::builder().build(manager).await.unwrap();
-
-    return pool;
+#[derive(Clone)]
+pub struct DB {
+    connection_string: Option<String>,
+    pub pool: Option<PostgresDBPool>,
 }
 
-pub async fn query<'a, T: From<Row>>(
-    DatabaseConnection(ref conn): DatabaseConnection,
-    query: &str,
-    query_params: QueryParams<'a>,
-) -> DBResult<Vec<T>> {
-    let params = query_params.unwrap_or(&[]);
+impl DB {
+    pub fn new() -> Self {
+        return Self {
+            connection_string: None,
+            pool: None,
+        };
+    }
 
-    let db_result = conn.query(query, params).await;
+    pub async fn setup(&self, connection_string: String) -> Self {
+        let mut self_clone = self.clone();
 
-    handle_query_debug(query, query_params);
+        let manager =
+            PostgresConnectionManager::new_from_stringlike(&connection_string, NoTls).unwrap();
 
-    match db_result {
-        Ok(rows) => {
-            let num_of_rows = rows.len();
+        let pool = Pool::builder().build(manager).await.unwrap();
 
-            if num_of_rows > 0 {
-                let mut results: Vec<T> = vec![];
+        self_clone.pool = Some(pool);
+        self_clone.connection_string = Some(connection_string);
 
-                for row in rows {
-                    let item = row.into();
-                    results.push(item);
+        return self_clone;
+    }
+
+    pub async fn query<'a, T: From<Row>, D: GenericClient>(
+        &self,
+        db: D,
+        query: &str,
+        query_params: QueryParams<'a>,
+    ) -> DBResult<Vec<T>> {
+        let params = query_params.unwrap_or(&[]);
+
+        let db_result = db.query(query, params).await;
+
+        Self::handle_query_debug(query, query_params);
+
+        match db_result {
+            Ok(rows) => {
+                let num_of_rows = rows.len();
+
+                if num_of_rows > 0 {
+                    let mut results: Vec<T> = vec![];
+
+                    for row in rows {
+                        let item = row.into();
+                        results.push(item);
+                    }
+
+                    return Ok(results);
                 }
 
-                return Ok(results);
+                return Err((404, String::from("No results were found")));
             }
-
-            return Err((StatusCode::NOT_FOUND, String::from("No results were found")));
-        }
-        Err(e) => {
-            let err = handle_db_error(e);
-            error!("DB Error: {}", err);
-            return Err((StatusCode::INTERNAL_SERVER_ERROR, err));
-        }
-    }
-}
-
-pub async fn query_single<'a, T: From<Row>>(
-    DatabaseConnection(ref conn): DatabaseConnection,
-    query: &str,
-    query_params: QueryParams<'a>,
-) -> DBResult<T> {
-    let params = query_params.unwrap_or(&[]);
-
-    let db_result = conn.query(query, params).await;
-
-    handle_query_debug(query, query_params);
-
-    match db_result {
-        Ok(rows) => {
-            let num_of_rows = rows.len();
-
-            if num_of_rows > 0 {
-                let item = rows[0].clone().into();
-                return Ok(item);
+            Err(e) => {
+                let err = Self::handle_db_error(e);
+                error!("DB Error: {}", err);
+                return Err((500, err));
             }
-
-            return Err((StatusCode::NOT_FOUND, String::from("No results were found")));
-        }
-        Err(e) => {
-            let err = handle_db_error(e);
-            error!("DB Error: {}", err);
-            return Err((StatusCode::INTERNAL_SERVER_ERROR, err));
         }
     }
-}
 
-pub async fn execute<'a>(
-    DatabaseConnection(ref conn): DatabaseConnection,
-    query: &str,
-    query_params: QueryParams<'a>,
-) -> Result<u64, (StatusCode, String)> {
-    let params = query_params.unwrap_or(&[]);
+    pub async fn query_single<'a, T: From<Row>, D: GenericClient>(
+        &self,
+        db: D,
+        query: &str,
+        query_params: QueryParams<'a>,
+    ) -> DBResult<T> {
+        let params = query_params.unwrap_or(&[]);
 
-    let db_result = conn.execute(query, params).await;
+        let db_result = db.query(query, params).await;
 
-    handle_query_debug(query, query_params);
+        Self::handle_query_debug(query, query_params);
 
-    match db_result {
-        Ok(rows) => Ok(rows),
-        Err(e) => {
-            let err = handle_db_error(e);
-            error!("DB Error: {}", err);
-            return Err((StatusCode::INTERNAL_SERVER_ERROR, err));
+        match db_result {
+            Ok(rows) => {
+                let num_of_rows = rows.len();
+
+                if num_of_rows > 0 {
+                    let item = rows[0].clone().into();
+                    return Ok(item);
+                }
+
+                return Err((400, String::from("No results were found")));
+            }
+            Err(e) => {
+                let err = Self::handle_db_error(e);
+                error!("DB Error: {}", err);
+                return Err((500, err));
+            }
         }
     }
-}
 
-fn handle_db_error(e: Error) -> String {
-    let db_error_option = e.as_db_error();
+    pub async fn execute<'a, D: GenericClient>(
+        db: D,
+        query: &str,
+        query_params: QueryParams<'a>,
+    ) -> DBResult<u64> {
+        let params = query_params.unwrap_or(&[]);
 
-    match db_error_option {
-        Some(err) => {
-            return err.message().to_string();
+        let db_result = db.execute(query, params).await;
+
+        Self::handle_query_debug(query, query_params);
+
+        match db_result {
+            Ok(rows) => Ok(rows),
+            Err(e) => {
+                let err = Self::handle_db_error(e);
+                error!("DB Error: {}", err);
+                return Err((500, err));
+            }
         }
-        None => return e.to_string(),
     }
-}
 
-fn handle_query_debug<'a>(query: &str, params_option: QueryParams<'a>) {
-    match params_option {
-        Some(params) => {
-            debug!("Query DB: `{}` with params: `{:?}`", query, params);
+    fn handle_db_error(e: Error) -> String {
+        let db_error_option = e.as_db_error();
+
+        match db_error_option {
+            Some(err) => {
+                return err.message().to_string();
+            }
+            None => return e.to_string(),
         }
-        None => debug!("Query DB: `{}`", query),
+    }
+
+    fn handle_query_debug<'a>(query: &str, params_option: QueryParams<'a>) {
+        match params_option {
+            Some(params) => {
+                debug!("Query DB: `{}` with params: `{:?}`", query, params);
+            }
+            None => debug!("Query DB: `{}`", query),
+        }
     }
 }
--- src/types.rs
+++ src/types.rs
@@ -1,45 +1,12 @@
-use axum::{
-    async_trait,
-    extract::{FromRef, FromRequestParts},
-    http::{request::Parts, StatusCode},
-};
 use bb8::{Pool, PooledConnection};
 use bb8_postgres::PostgresConnectionManager;
-use serde::Deserialize;
 use tokio_postgres::{types::ToSql, NoTls, Row};
 
 pub type PostgresDBPool = Pool<PostgresConnectionManager<NoTls>>;
-pub type DBResult<T> = Result<T, (StatusCode, String)>;
+pub type PostgresDB = PostgresConnectionManager<NoTls>;
+
+pub type DBResult<T> = Result<T, (u16, String)>;
 pub type QueryParams<'a> = Option<&'a [&'a (dyn ToSql + Sync)]>;
 pub type DBRow = Row;
-pub struct DatabaseConnection(pub PooledConnection<'static, PostgresConnectionManager<NoTls>>);
-#[derive(Deserialize)]
-pub struct DB {
-    pub host: String,
-    pub user: String,
-    pub password: String,
-}
 
-#[async_trait]
-impl<S> FromRequestParts<S> for DatabaseConnection
-where
-    PostgresDBPool: FromRef<S>,
-    S: Send + Sync,
-{
-    type Rejection = (StatusCode, String);
-
-    async fn from_request_parts(_parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
-        let pool = PostgresDBPool::from_ref(state);
-
-        let conn = pool.get_owned().await.map_err(internal_error)?;
-
-        Ok(Self(conn))
-    }
-}
-
-pub fn internal_error<E>(err: E) -> (StatusCode, String)
-where
-    E: std::error::Error,
-{
-    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
-}
+pub type ConnectionPool = PooledConnection<'static, PostgresConnectionManager<NoTls>>;
